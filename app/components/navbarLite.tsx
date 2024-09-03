4323c3c8ac1329e477c4821acb10805509e0ef61
bayufadayan <bayufadayan@gmail.com>
2024-09-03 10:24:10 +0700

    starting convert design from html

---
+++ app/components/navbarLite.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function NavbarLite() {
+  return (
+    <nav className="login-nav">
+        <Image src="images/navbar-flat.svg" alt="logo instansi" width={200} height={34} />
+      </nav>
+  );
+}
---
+++ app/layouts/login/page.tsx
@@ -0,0 +1,10 @@
+import NavbarLite from "@/app/components/navbarLite"
+
+export default function LoginPage() {
+
+    return (
+        <div>
+            <NavbarLite />
+        </div>
+    )
+}
