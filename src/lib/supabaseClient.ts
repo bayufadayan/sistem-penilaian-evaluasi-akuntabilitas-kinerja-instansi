66d27c3bdb02a935823d89cbdf55dbb8154bd556
bayufadayan <bayufadayan@gmail.com>
2024-09-16 11:46:26 +0700

    add supabaseClient

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,23 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+// fungsi upload
+export const uploadFileToSupabase = async (file: File) => {
+  try {
+    const { data, error } = await supabase.storage
+      .from("evidence") // Gunakan bucket "evidence"
+      .upload(`public/${file.name}`, file);
+
+    if (error) throw error;
+
+    return data; // Kembalikan informasi file, termasuk file path
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    throw error;
+  }
+};
+
