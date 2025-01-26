149a061b8822fca71c97b749281539dedaac6859
bayufadayan <bayufadayan@gmail.com>
2025-01-26 14:50:47 +0700

    update UI to coloring selected sub-component

--- src/components/subComponent.tsx
+++ src/components/subComponent.tsx
@@ -1,5 +1,7 @@
+"use client"
 import "@/styles/styles.module.css";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface SubComponent {
   id: number;
@@ -14,15 +16,23 @@ export default function SubComponentListCard({
   evaluationId
 }: {
   subComponents: SubComponent[];
-  evaluationId : string
+  evaluationId: string
 }) {
+
+  const pathname = usePathname();
+  const pathSegments = pathname.split('/');
+  const subcomponentId = pathSegments[pathSegments.length - 1];
+
   return subComponents.sort(
     (a: SubComponent, b: SubComponent) =>
       a.subcomponent_number - b.subcomponent_number
   ).map((subComponent) => (
     <Link href={`/sheets/${evaluationId}/${subComponent.id}`} key={subComponent.id}>
       <p
-        className="font-medium hover:bg-slate-600 hover:text-white hover:cursor-pointer"
+        className={`font-medium hover:text-white hover:cursor-pointer ${subcomponentId === String(subComponent.id)
+            ? "bg-green-600 text-white"
+            : "bg-blue-100 hover:bg-slate-600"
+          }`}
       >
         {subComponent.name}
       </p>
